80032cc9cb1dff712e0cf0bbe61b5048086aad1b
BuildTools <unconfigured@null.spigotmc.org>
2020-04-13 16:01:31 +0900

    domain패키지, repository 패키지 추가

---
+++ src/main/java/com/sehyeon/app/domain/Student.java
@@ -0,0 +1,61 @@
+package com.sehyeon.app.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private long phoneNo;
+
+    public Student(String name, String email, long phoneNo) {
+        this.name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+    }
+
+    public Student() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
